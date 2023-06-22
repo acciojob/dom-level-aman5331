00c2610594877661651b9a161f9713308c711007
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-06-22 13:08:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const element = document.getElementById('level');
+
+// Initialize the DOM level variable
+let domLevel = 0;
+
+// Traverse up the DOM tree until reaching the topmost parent
+let parent = element.parentNode;
+while (parent !== null) {
+  domLevel++;
+  parent = parent.parentNode;
+}
+
+// Display the DOM level using alert()
+alert("The level of the element is: " + domLevel);
